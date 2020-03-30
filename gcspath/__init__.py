529f63026abe1b11c3336febb152a030e28a85ef
justindujardin <justin@dujardinconsulting.com>
2020-03-30 16:33:11 -0700

    fix(gcs): gracefully handle invalid gcs client case
     - by default ignore default client credential errors until the accessor.client prop is referenced.
     - this allows you to call `use_fs` after the initial module import

--- gcspath/__init__.py
+++ gcspath/__init__.py
@@ -1,12 +1,14 @@
-from .api import GCSPath, PureGCSPath  # noqa
-from .client import BucketEntry, BucketStat  # noqa
 from .api import (  # noqa
-    get_fs_client,
-    use_fs,
     BucketClient,
+    BucketsAccessor,
     ClientBlob,
     ClientBucket,
     ClientError,
+    GCSPath,
+    PureGCSPath,
+    get_fs_client,
+    use_fs,
 )
+from .client import BucketEntry, BucketStat  # noqa
 from .file import BucketClientFS, BucketEntryFS, ClientBlobFS, ClientBucketFS  # noqa
 from .gcs import BucketClientGCS, BucketEntryGCS, ClientBlobGCS, ClientBucketGCS  # noqa
--- tests/conftest.py
+++ tests/conftest.py
@@ -6,9 +6,14 @@ from gcspath import use_fs
 
 
 @pytest.fixture()
-def with_fs():
+def temp_folder():
     tmp_dir = tempfile.mkdtemp()
     yield Path(tmp_dir)
     shutil.rmtree(tmp_dir)
+
+
+@pytest.fixture()
+def with_fs(temp_folder):
+    yield temp_folder
     # Turn off FS adapter
     use_fs(False)
