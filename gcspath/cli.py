7b85ca15e9f6deee941e934d42d57562aab7e5d9
justindujardin <justin@dujardinconsulting.com>
2020-04-23 17:36:49 -0700

    test(cli): add tests for cp/mv files and folders

--- gcspath/cli.py
+++ gcspath/cli.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3.7
 from pathlib import Path
-from typing import Union
 
 import typer
-from gcspath import GCSPath
+from gcspath import GCSPath, FluidPath
 
 app = typer.Typer()
 
@@ -13,21 +12,10 @@ def cp(from_location: str, to_location: str):
     """
     Copy a blob or folder of blobs from one bucket to another.
     """
-    from_path: Union[Path, GCSPath]
-    to_path: Union[Path, GCSPath]
-
-    try:
-        from_path = GCSPath(from_location)
-    except ValueError:
-        from_path = Path(from_location)
+    from_path: FluidPath = GCSPath.fluid(from_location)
     if not from_path.exists():
         raise ValueError(f"from_path is not an existing Path or GCSPath: {from_path}")
-
-    try:
-        to_path = GCSPath(to_location)
-    except ValueError:
-        to_path = Path(to_location)
-
+    to_path: FluidPath = GCSPath.fluid(to_location)
     if from_path.is_dir():
         to_path.mkdir(parents=True, exist_ok=True)
         for blob in from_path.rglob("*"):
@@ -36,6 +24,12 @@ def cp(from_location: str, to_location: str):
             to_blob = to_path / str(blob.relative_to(from_path))
             to_blob.write_bytes(blob.read_bytes())
     elif from_path.is_file():
+        # Copy prefix from the source if the to_path has none.
+        #
+        # e.g. "cp ./file.txt gs://bucket-name/" writes "gs://bucket-name/file.txt"
+        if isinstance(to_path, GCSPath) and to_path.prefix == "":
+            to_path = to_path / from_path
+
         to_path.parent.mkdir(parents=True, exist_ok=True)
         to_path.write_bytes(from_path.read_bytes())
 
@@ -45,21 +39,10 @@ def mv(from_location: str, to_location: str):
     """
     Move a blob or folder of blobs from one path to another.
     """
-    from_path: Union[Path, GCSPath]
-    to_path: Union[Path, GCSPath]
-
-    try:
-        from_path = GCSPath(from_location)
-    except ValueError:
-        from_path = Path(from_location)
+    from_path: FluidPath = GCSPath.fluid(from_location)
     if not from_path.exists():
         raise ValueError(f"from_path is not an existing Path or GCSPath: {from_path}")
-
-    try:
-        to_path = GCSPath(to_location)
-    except ValueError:
-        to_path = Path(to_location)
-
+    to_path: FluidPath = GCSPath.fluid(to_location)
     if from_path.is_dir():
         to_path.mkdir(parents=True, exist_ok=True)
         to_unlink = []
@@ -71,7 +54,14 @@ def mv(from_location: str, to_location: str):
             to_unlink.append(blob)
         for blob in to_unlink:
             blob.unlink()
+        if from_path.is_dir():
+            from_path.rmdir()
     elif from_path.is_file():
+        # Copy prefix from the source if the to_path has none.
+        #
+        # e.g. "cp ./file.txt gs://bucket-name/" writes "gs://bucket-name/file.txt"
+        if isinstance(to_path, GCSPath) and to_path.prefix == "":
+            to_path = to_path / from_path
         to_path.parent.mkdir(parents=True, exist_ok=True)
         to_path.write_bytes(from_path.read_bytes())
         from_path.unlink()
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,30 +10,99 @@ runner = CliRunner()
 
 
 @pytest.mark.parametrize("adapter", TEST_ADAPTERS)
-def test_cp_files(with_adapter):
-    source = "gs://gcspath-tests-1/cli_cp.txt"
-    destination = "gs://gcspath-tests-2/cli_cp.txt"
-    content = "hi"
-    dest_path = GCSPath(destination)
-    source_path = GCSPath(source)
-    source_path.write_text(content)
+def test_cli_cp_file(with_adapter, bucket: str):
+    source = f"gs://{bucket}/cli_cp_file/file.txt"
+    destination = f"gs://{bucket}/cli_cp_file/other.txt"
+    GCSPath(source).write_text("---")
     assert runner.invoke(app, ["cp", source, destination]).exit_code == 0
-    assert dest_path.is_file()
-    # Both files exist
-    assert source_path.read_text() == content
-    assert dest_path.read_text() == content
+    assert GCSPath(source).exists()
+    assert GCSPath(destination).is_file()
 
 
 @pytest.mark.parametrize("adapter", TEST_ADAPTERS)
-def test_cp_folders(with_adapter):
-    source = "gs://gcspath-tests-1/cli_cp.txt"
-    destination = "gs://gcspath-tests-2/cli_cp.txt"
-    content = "hi"
-    dest_path = GCSPath(destination)
-    source_path = GCSPath(source)
-    source_path.write_text(content)
-    assert runner.invoke(app, ["cp", source, destination]).exit_code == 0
-    assert dest_path.is_file()
-    # Both files exist
-    assert source_path.read_text() == content
-    assert dest_path.read_text() == content
+def test_cli_cp_folder(with_adapter, bucket: str):
+    root = GCSPath.from_bucket(bucket)
+    source = root / "cli_cp_folder"
+    destination = root / "cli_cp_folder_other"
+    for i in range(2):
+        for j in range(2):
+            (source / f"{i}" / f"{j}").write_text("---")
+    assert runner.invoke(app, ["cp", str(source), str(destination)]).exit_code == 0
+    assert GCSPath(source).exists()
+    assert GCSPath(destination).is_dir()
+    for i in range(2):
+        for j in range(2):
+            assert (destination / f"{i}" / f"{j}").is_file()
+
+
+@pytest.mark.parametrize("adapter", TEST_ADAPTERS)
+def test_cli_mv_folder(with_adapter, bucket: str):
+    root = GCSPath.from_bucket(bucket)
+    source = root / "cli_mv_folder"
+    destination = root / "cli_mv_folder_other"
+    for i in range(2):
+        for j in range(2):
+            (source / f"{i}" / f"{j}").write_text("---")
+    assert runner.invoke(app, ["mv", str(source), str(destination)]).exit_code == 0
+    assert not GCSPath(source).exists()
+    assert GCSPath(destination).is_dir()
+    # Ensure source files are gone
+    for i in range(2):
+        for j in range(2):
+            assert not (source / f"{i}" / f"{j}").is_file()
+    # And dest files exist
+    for i in range(2):
+        for j in range(2):
+            assert (destination / f"{i}" / f"{j}").is_file()
+
+
+@pytest.mark.parametrize("adapter", TEST_ADAPTERS)
+def test_cli_mv_file(with_adapter, bucket: str):
+    source = f"gs://{bucket}/cli_mv_file/file.txt"
+    destination = f"gs://{bucket}/cli_mv_file/other.txt"
+    GCSPath(source).write_text("---")
+    assert GCSPath(source).exists()
+    assert runner.invoke(app, ["mv", source, destination]).exit_code == 0
+    assert not GCSPath(source).exists()
+    assert GCSPath(destination).is_file()
+
+
+# @pytest.mark.parametrize("adapter", TEST_ADAPTERS)
+# def test_cli_rename_files_across_buckets(with_adapter, bucket: str, other_bucket: str):
+#     # Rename a single file across buckets
+#     GCSPath(f"gs://{bucket}/rename/file.txt").write_text("---")
+#     GCSPath(f"gs://{bucket}/rename/file.txt").rename(
+#         f"gs://{other_bucket}/rename/other.txt"
+#     )
+#     assert not GCSPath(f"gs://{bucket}/rename/file.txt").exists()
+#     assert GCSPath(f"gs://{other_bucket}/rename/other.txt").is_file()
+
+
+# @pytest.mark.parametrize("adapter", TEST_ADAPTERS)
+# def test_cli_rename_folders_in_bucket(with_adapter, bucket: str):
+#     # Rename a folder in the same bucket
+#     GCSPath(f"gs://{bucket}/rename/folder/one.txt").write_text("---")
+#     GCSPath(f"gs://{bucket}/rename/folder/two.txt").write_text("---")
+#     path = GCSPath(f"gs://{bucket}/rename/folder/")
+#     new_path = GCSPath(f"gs://{bucket}/rename/other/")
+#     path.rename(new_path)
+#     assert not path.exists()
+#     assert new_path.exists()
+#     assert GCSPath(f"gs://{bucket}/rename/other/one.txt").is_file()
+#     assert GCSPath(f"gs://{bucket}/rename/other/two.txt").is_file()
+
+
+# @pytest.mark.parametrize("adapter", TEST_ADAPTERS)
+# def test_cli_rename_folders_across_buckets(
+#     with_adapter, bucket: str, other_bucket: str
+# ):
+#     # Rename a folder across buckets
+#     GCSPath(f"gs://{bucket}/rename/folder/one.txt").write_text("---")
+#     GCSPath(f"gs://{bucket}/rename/folder/two.txt").write_text("---")
+#     path = GCSPath(f"gs://{bucket}/rename/folder/")
+#     new_path = GCSPath(f"gs://{other_bucket}/rename/other/")
+#     path.rename(new_path)
+#     assert not path.exists()
+#     assert new_path.exists()
+#     assert GCSPath(f"gs://{other_bucket}/rename/other/one.txt").is_file()
+#     assert GCSPath(f"gs://{other_bucket}/rename/other/two.txt").is_file()
