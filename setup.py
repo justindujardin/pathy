0f7b73bc27b44c11b31c4fa4ecee2c97429f2383
justindujardin <justin@dujardinconsulting.com>
2021-04-21 11:20:31 -0700

    chore: add py 3.9 to setup.py classifiers

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ def setup_package():
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
         ],
     )
 
