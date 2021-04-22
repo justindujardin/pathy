e61fb01f19e44c78dca457e11c45ed44307a00da
justindujardin <justin@dujardinconsulting.com>
2021-04-22 11:13:06 -0700

    chore: cleanup from review

--- pathy/_tests/conftest.py
+++ pathy/_tests/conftest.py
@@ -15,7 +15,10 @@ from . import has_gcs
 has_credentials = "GCS_CREDENTIALS" in os.environ
 
 # Which adapters to use
-TEST_ADAPTERS = ["gcs"] if has_credentials and has_gcs else ["fs"]
+TEST_ADAPTERS = ["gcs", "fs"] if has_credentials and has_gcs else ["fs"]
+# A unique identifier used to allow each python version and OS to test
+# with separate bucket paths. This makes it possible to parallelize the
+# tests.
 ENV_ID = f"{sys.platform}.{sys.version_info.major}.{sys.version_info.minor}"
 
 
