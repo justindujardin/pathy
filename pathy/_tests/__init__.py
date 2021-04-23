5bb7e1b8faa422c8a80d91d7d8e71f6de4052962
Justin DuJardin <justindujardin@users.noreply.github.com>
2021-04-23 16:13:54 -0700

    feat(clients): add support for S3 bucket storage (#54)
    * feat(clients): support for "s3://bucket/blobs"
    
     - install with `pip install pathy[s3]` extras
     - update tests to substitute the scheme where needed instead of hardcoded "gs"
    
    * chore: cleanup from review
    
    * test(s3): exercise list_buckets and pagination codepaths
    
    * chore: fix passing credentials through the env
    
    * chore: fix leftover dev change in conftest
    
    * chore: tighten up missing extras tests
    
     - specifically disable use_fs incase it was still set from another test
    
    * chore: fix passing s3 creds to smart_open
    
    * chore: cleanup from review
    
     - add env vars for s3
    
    * chore: cleanup from review
    
    * chore(readme): add cloud platform support table

--- pathy/_tests/__init__.py
+++ pathy/_tests/__init__.py
@@ -5,3 +5,11 @@ try:
     has_gcs = bool(BucketClientGCS)
 except ImportError:
     has_gcs = False
+
+has_s3: bool
+try:
+    from ..s3 import BucketClientS3
+
+    has_s3 = bool(BucketClientS3)
+except ImportError:
+    has_s3 = False
