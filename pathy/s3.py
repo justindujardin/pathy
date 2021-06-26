97187528b70fdb96e928e1f90a3b06732e126b16
Justin DuJardin <justindujardin@users.noreply.github.com>
2021-06-26 13:12:39 -0700

    feat: support smart_open >=5.0.0,<6.0.0 (#63)
    * feat: increase smart_open version range to < 6.0.0
    * chore: update s3 session for smart_open >= 5
    
    BREAKING CHANGE: This change removes support for smart_open < 5.0.0
    
    The API for specifying s3 credentials changed in smart_open v5, so previous versions are incompatible.

--- pathy/s3.py
+++ pathy/s3.py
@@ -112,11 +112,13 @@ class BucketS3(Bucket):
 
 class BucketClientS3(BucketClient):
     client: S3NativeClient
-    _session: Optional[Any]
+    _session: Optional[boto3.Session]
 
     @property
     def client_params(self) -> Dict[str, Any]:
-        return dict() if self._session is None else dict(session=self._session)
+        session: Any = self._session
+        result: Any = dict() if session is None else dict(client=session.client("s3"))
+        return result
 
     def __init__(self, **kwargs: Any) -> None:
         self.recreate(**kwargs)
