cdc1efbf387d2a406707755ee5bb554b5d307653
Jongwook Choi <wookayin@gmail.com>
2022-11-10 12:04:11 -0500

    Add python 3.10 and 3.11 tests to CI.

--- setup.py
+++ setup.py
@@ -54,6 +54,8 @@ def setup_package():
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
             "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
         ],
     )
 
